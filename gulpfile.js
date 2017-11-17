65856d7ced3802a258c1b0dacba4b46d24c2b5fe
Fabian <pecherfabian@gmail.com>
2017-11-17 21:19:59 +0100

    added basic grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
-var gulp  = require('gulp');
-var sass  = require('gulp-sass');
+var gulp  = require('gulp'),
+    sass  = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    livereload = require('gulp-livereload')
 
 gulp.task('styles', function() {
-    gulp.src('scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+  return gulp.src('scss/grid.scss')
+      	.pipe(sass({ style: 'expanded' }))
+      	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+      	.pipe(gulp.dest('css/'))
+        .pipe(livereload());
 });
 
 
 //Watch task
-gulp.task('watch',function() {
-    gulp.watch('scss/**/*.scss',['styles']);
+gulp.task('watch', function() {
+
+  livereload.listen();
+
+  // Watch .scss files
+  gulp.watch('scss/**/*.scss', ['styles']);
+
 });
+
+gulp.task('default', ['styles', 'watch']);
