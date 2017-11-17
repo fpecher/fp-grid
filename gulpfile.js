e528be07a1023aa620c20cf807fdd0bda02586fd
Fabian <pecherfabian@gmail.com>
2017-11-17 23:28:09 +0100

    added build task to build a minified version of the grid.

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
 var gulp  = require('gulp'),
     sass  = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
-    livereload = require('gulp-livereload')
+    minifyCss = require('gulp-minify-css'),
+    rename = require('gulp-rename')
 
 gulp.task('styles', function() {
-  return gulp.src('scss/grid.scss')
-      	.pipe(sass({ style: 'expanded' }))
-      	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-      	.pipe(gulp.dest('css/'))
-        .pipe(livereload());
+  return gulp.src('scss/*.scss')
+      	.pipe(sass({ style: 'compact' }))
+      	.pipe(autoprefixer('last 2 version', 'safari 10', 'ie 11', 'opera 12.1', 'ios 6', 'android 4'));
 });
 
+gulp.task('build', function () {
+  return gulp.src('scss/*.scss')
+          .pipe(sass({ style: 'compact' }))
+          .pipe(rename({
+            basename : 'grid',
+            extname : '.min.css'
+          }))
+          .pipe(minifyCss())
+          .pipe(gulp.dest('css/'));
+});
 
-//Watch task
 gulp.task('watch', function() {
-
-  livereload.listen();
-
-  // Watch .scss files
   gulp.watch('scss/**/*.scss', ['styles']);
-
 });
 
 gulp.task('default', ['styles', 'watch']);
